d75acce71a6b9beffd933cc56770a39ed1f3632b
Tiffany Wu <tiffwu88@gmail.com>
2018-03-21 12:30:26 -0400

    Current version to transfer to vcm

--- test_client.py
+++ test_client.py
@@ -3,4 +3,8 @@ import client
 
 
 def validate_heart_rate_requests(r):
-    pass
+    data = {
+        "user_email": "suyash@suyashkumar.com",
+        "user_age": 50, // in years
+        "heart_rate": 100
+    }
